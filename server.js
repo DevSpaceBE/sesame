fba4f16adc5cc592bb91a9bdfe9fa59d3c1f322b
Julien Biezemans <jb@jbpros.com>
2012-03-09 22:59:44 +0100

    Add logging to server

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var INBOUND_DATA  = ">";
+var OUTBOUND_DATA = "<";
+
 var url        = require('url');
 var connect    = require('connect');
 var parsers    = require('serialport').parsers;
@@ -7,22 +10,29 @@ var port         = process.env.PORT || 2847;
 var serialDevice = process.env.SERIAL_DEVICE || '/dev/tty-usbserial1';
 var server       = connect.createServer();
 
-server.use(connect.static(__dirname));
 server.use(function(req, res, next){
   var pathname = url.parse(req.url).pathname;
+  var remote   = req.connection.remoteAddress;
+
   if (pathname == '/open') {
+    log(remote, INBOUND_DATA, "open gate request");
     openGate(function(err) {
       if (err) {
         res.writeHead(500);
         res.end("Could not open the gate :(");
+        log(req.connection.remoteAddress, OUTBOUND_DATA, "open gate failure (" + err + ")");
       } else {
         res.end("Opening the gate for you...");
+        log(req.connection.remoteAddress, OUTBOUND_DATA, "open gate success");
       }
     });
   } else {
+    if (pathname == '/')
+      log(remote, INBOUND_DATA, "application load (static)");
     next();
   }
-})
+});
+server.use(connect.static(__dirname));
 
 /////////////////////
 
@@ -53,6 +63,16 @@ function openGate(callback) {
     }
   });
 };
+
+function log(remote, dataDirection, message) {
+  console.log(now() + ' - ' + remote + ' ' + dataDirection + ' ' + message);
+};
+
+function now(d) {
+  function pad(n) { return n<10 ? '0' + n : n; }
+  var d = new Date();
+  return d.getFullYear() + '-' + pad(d.getMonth()) + '-' + pad(d.getDate()) + ' ' + pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds());
+}
 
 init(function(err) {
   if (err)
