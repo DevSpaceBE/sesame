117b69c6a0990bab77ab66f8a114a634351c8e5a
Julien Biezemans <jb@jbpros.com>
2012-03-11 12:19:03 +0100

    Extract Sesame logic into its own domain object

--- server.js
+++ server.js
@@ -1,65 +1,35 @@
-var url        = require('url');
-var express    = require('express');
-var parsers    = require('serialport').parsers;
-var SerialPort = require('serialport').SerialPort;
+(function main() {
+  var Sesame  = require('./lib/sesame');
+  var express = require('express');
 
-var port         = process.env.PORT || 2847;
-var serialDevice = process.env.SERIAL_DEVICE || '/dev/tty-usbserial1';
-var app          = express.createServer();
+  var port   = process.env.PORT || 2847;
+  var app    = express.createServer();
 
-app.configure(function() {
-  app.use(express.logger());
-  app.use(app.router);
-  app.use(express.static(__dirname));
-});
+  app.configure(function() {
+    app.use(express.logger());
+    app.use(app.router);
+    app.use(express.static(__dirname));
 
-app.get('/open', function(req, res) {
-  openGate(function(err) {
-    if (err) {
-      res.writeHead(500);
-      res.end("Could not open the gate :(");
-    } else {
-      res.writeHead(200);
-      res.end("Opening the gate for you...");
-    }
+    var sesame = new Sesame();
+    sesame.check(function(err) {
+      if (err)
+        console.log("Warning: " + err + "\n");
+    });
   });
-});
 
-
-/////////////////////
-
-function init(callback) {
-  getSerialPort(callback);
-}
-
-function getSerialPort(callback) {
-  var serialPort;
-
-  var error = void(0);
-  try {
-    serialPort = new SerialPort(serialDevice, {baudrate: 9600});
-  } catch (err) {
-    error = "Could not open serial port on " + serialDevice;
-  }
-  callback(error, serialPort);
-};
-
-function openGate(callback) {
-  var serialPort = getSerialPort(function(err, serialPort) {
-    if (err) {
-      callback(err);
-    } else {
-      serialPort.write('1');
-      serialPort.close();
-      callback();
-    }
+  app.get('/open', function(req, res) {
+    var sesame = new Sesame();
+    sesame.open(function(err) {
+      if (err) {
+        res.writeHead(500);
+        res.end("Could not open the gate :(");
+      } else {
+        res.writeHead(200);
+        res.end("Opening the gate for you...");
+      }
+    });
   });
-};
 
-init(function(err) {
-  if (err)
-    console.log("Warning: " + err + "\n");
   app.listen(port);
   console.log('Accepting connections on port '+port+'...');
-});
-
+})();
